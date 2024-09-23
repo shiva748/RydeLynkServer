9485e80c39f9a1fd7a9af2981e14f7bc0c83600c
shiva748 <shivagautam2002@gmail.com>
2024-09-23 18:11:18 +0530

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const { createServer } = require("node:http");
-const port = 3006 || process.env.PORT;
+const port = 3005 || process.env.PORT;
 const Routes = require("./Route/Routes");
 const Adminroutes = require("./Route/adminroute");
 const bodyParser = require("body-parser");
